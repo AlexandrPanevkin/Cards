134281628f05e58dcaa74fe59cc4068012374080
AlexanderPanevkin <panevkin900@gmail.com>
2023-08-13 20:30:51 +0300

    feat: add table decks

--- src/pages/decks/decks.tsx
+++ src/pages/decks/decks.tsx
@@ -1,13 +1,34 @@
-import { useLazyGetDecksQuery } from '../../services/decks/decks-api.ts'
+import { useGetDecksQuery } from '../../services/decks/decks-api.ts'
 
 export const Decks = () => {
-  const [initializeQuery, { data, isLoading }] = useLazyGetDecksQuery()
+  const { data, isLoading } = useGetDecksQuery({
+    itemsPerPage: 20,
+  })
+
+  if (isLoading) return <div>Loading...</div>
 
   return (
     <div>
-      isLoading: {isLoading.toString()}
-      <button onClick={() => initializeQuery()}>fetch</button>
-      {JSON.stringify(data)}
+      <table>
+        <tbody>
+          <tr>
+            <th>Name</th>
+            <th>Cards</th>
+            <th>Last updated</th>
+            <th>Created by</th>
+          </tr>
+          {data?.items.map(deck => {
+            return (
+              <tr key={deck.id}>
+                <td>{deck.name}</td>
+                <td>{deck.cardsCount}</td>
+                <td>{new Date(deck.updated).toLocaleDateString('ru-Ru')}</td>
+                <td>{deck.author.name}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/services/decks/decks-api.ts
+++ src/services/decks/decks-api.ts
@@ -3,11 +3,61 @@ import { baseApi } from '../base-api.ts'
 const decksApi = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
-      getDecks: builder.query<any, void>({
-        query: () => `v1/decks`,
+      getDecks: builder.query<DecksResponse, GetDecksArgs>({
+        query: args => {
+          return {
+            url: `v1/decks`,
+            method: 'GET',
+            params: args,
+          }
+        },
       }),
     }
   },
 })
 
+type GetDecksArgs = {
+  minCardsCount?: number
+  maxCardsCount?: number
+  name?: string
+  authorId?: string
+  orderBy?: string
+  currentPage?: number
+  itemsPerPage?: number
+}
+
 export const { useGetDecksQuery, useLazyGetDecksQuery } = decksApi
+
+export interface Pagination {
+  totalPages: number
+  currentPage: number
+  itemsPerPage: number
+  totalItems: number
+}
+
+export interface Author {
+  id: string
+  name: string
+}
+
+export interface Deck {
+  id: string
+  userId: string
+  name: string
+  isPrivate: boolean
+  shots: number
+  cover?: string | null
+  rating: number
+  isDeleted?: boolean | null
+  isBlocked?: boolean | null
+  created: string
+  updated: string
+  cardsCount: number
+  author: Author
+}
+
+export interface DecksResponse {
+  maxCardsCount: number
+  pagination: Pagination
+  items: Deck[]
+}
