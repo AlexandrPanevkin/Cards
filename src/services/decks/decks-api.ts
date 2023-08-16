470584c8fd14ba87fbcb2d1c07b22dc34aa121e4
AlexanderPanevkin <panevkin900@gmail.com>
2023-08-16 21:49:39 +0300

    feat: add type for decks

--- src/services/decks/decks-api.ts
+++ src/services/decks/decks-api.ts
@@ -1,5 +1,7 @@
 import { baseApi } from '../base-api.ts'
 
+import { DecksResponse, GetDecksArgs } from './types.ts'
+
 const decksApi = baseApi.injectEndpoints({
   endpoints: builder => {
     return {
@@ -27,48 +29,4 @@ const decksApi = baseApi.injectEndpoints({
   },
 })
 
-type GetDecksArgs = {
-  minCardsCount?: number
-  maxCardsCount?: number
-  name?: string
-  authorId?: string
-  orderBy?: string
-  currentPage?: number
-  itemsPerPage?: number
-}
-
 export const { useGetDecksQuery, useCreateDeckMutation } = decksApi
-
-export interface Pagination {
-  totalPages: number
-  currentPage: number
-  itemsPerPage: number
-  totalItems: number
-}
-
-export interface Author {
-  id: string
-  name: string
-}
-
-export interface Deck {
-  id: string
-  userId: string
-  name: string
-  isPrivate: boolean
-  shots: number
-  cover?: string | null
-  rating: number
-  isDeleted?: boolean | null
-  isBlocked?: boolean | null
-  created: string
-  updated: string
-  cardsCount: number
-  author: Author
-}
-
-export interface DecksResponse {
-  maxCardsCount: number
-  pagination: Pagination
-  items: Deck[]
-}
