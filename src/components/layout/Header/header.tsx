72ec746915d93d9ab0076d1c772a1d360d035dfa
AlexanderPanevkin <panevkin900@gmail.com>
2023-09-06 17:06:29 +0300

    feat: add Cards image

---
+++ src/components/layout/Header/header.tsx
@@ -0,0 +1,5 @@
+import s from './header.module.scss'
+import { Cards } from './icons/Cards.tsx'
+export const Header = () => {
+  return <Cards className={s.cards} />
+}
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from './Header/header.tsx'
+
 export const Layout = () => {
   return (
     <div>
+      <Header />
       <Outlet />
     </div>
   )
