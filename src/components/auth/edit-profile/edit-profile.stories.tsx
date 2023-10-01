15589928b215e0c90d5f9aa913237a14879043ba
AlexanderPanevkin <panevkin900@gmail.com>
2023-10-01 19:34:45 +0300

    fix: edit profile story

--- src/components/auth/edit-profile/edit-profile.stories.tsx
+++ src/components/auth/edit-profile/edit-profile.stories.tsx
@@ -9,23 +9,23 @@ const meta = {
   title: 'Auth/Edit-profile',
   component: EditProfile,
   tags: ['autodocs'],
-  argTypes: {
-    data: {
-      avatar: '',
-      id: '',
-      isEmailVerified: false,
-      created: '',
-      updated: '',
-      name: 'sdfsdfsdf',
-      email: 'sdsdsd@mail.com',
-    },
-  },
 } satisfies Meta<typeof EditProfile>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {},
+  args: {
+    data: {
+      avatar:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV9JqkBrfwaFEIRaVhyYtwjifjGJJwjT-lOx8QgN6XRe9T1UmUWXe17Tc5ZVLMRwmJeRE&usqp=CAU',
+      id: '',
+      isEmailVerified: false,
+      created: '',
+      updated: '',
+      name: 'My name',
+      email: 'Email@mail.com',
+    },
+  },
   decorators: [Story => <Provider store={store}>{Story()}</Provider>],
 }
