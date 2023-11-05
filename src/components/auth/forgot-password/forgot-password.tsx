48d0b1b7d3e84333614c989babd1a69a143106b0
AlexanderPanevkin <panevkin900@gmail.com>
2023-11-05 20:24:56 +0300

    feat: add forgot-password.module.scss, index.ts

--- src/components/auth/forgot-password/forgot-password.tsx
+++ src/components/auth/forgot-password/forgot-password.tsx
@@ -1,18 +1,51 @@
 import { FC } from 'react'
 
-import { Card } from '../../ui/card'
-import { Typography } from '../../ui/typography'
-import s from '../sign-in/sign-in.module.scss'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
+import { Card } from '../../ui/card'
+import { ControlledTextField } from '../../ui/controlled/controlled-text-field'
+import { Typography } from '../../ui/typography'
+import { forgotPasswordScheme } from '../validation/forgot-password-scheme.ts'
+
+import s from './forgot-password.module.scss'
+
+type ForgotPasswordType = z.infer<typeof forgotPasswordScheme>
 type ForgotPasswordPropsType = {
   onSubmit: (email: string) => void
 }
 export const ForgotPassword: FC<ForgotPasswordPropsType> = ({ onSubmit }) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<ForgotPasswordType>({
+    resolver: zodResolver(forgotPasswordScheme),
+    mode: 'onSubmit',
+  })
+  const onSubmitForm = handleSubmit(data => {
+    onSubmit(data.email)
+  })
+
   return (
     <Card>
-      <Typography variant={'large'} className={s.signIn}>
+      <Typography variant={'large'} className={s.forgotPassword}>
         Forgot your password?
       </Typography>
+      <form onSubmit={onSubmitForm}>
+        <ControlledTextField
+          name={'email'}
+          control={control}
+          label={'Email'}
+          type={'text'}
+          errorMessage={errors.email?.message}
+          className={s.inputMargin}
+        />
+        <Typography variant={'body2'} className={s.forgotPassword}>
+          Enter your email address and we will send you further instructions
+        </Typography>
+      </form>
     </Card>
   )
 }
