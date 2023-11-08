08b9146e5cdb334c6455a7a59e517d80b8b7fc49
AlexanderPanevkin <panevkin900@gmail.com>
2023-11-08 20:20:22 +0300

    feat: add CheckEmailSvg

--- src/components/auth/check-email/check-email.tsx
+++ src/components/auth/check-email/check-email.tsx
@@ -1,5 +1,18 @@
-import { Card } from '../../ui/card'
+import { FC } from 'react'
 
-export const CheckEmail = () => {
-  return <Card></Card>
+import { CheckEmailSvg } from '../../../assets/icons/CheckEmailSvg.tsx'
+import { Card } from '../../ui/card'
+import { Typography } from '../../ui/typography'
+
+type CheckEmailPropsType = {
+  email: string
+}
+
+export const CheckEmail: FC<CheckEmailPropsType> = () => {
+  return (
+    <Card>
+      <Typography variant={'large'}>Check Email</Typography>
+      <CheckEmailSvg />
+    </Card>
+  )
 }
